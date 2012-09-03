1d9a7a9a9618e3b91c9d856b734c6cb69f5ba967
twacker <twacker@mymail.mines.edu>
2012-09-02 19:42:20 -0600

    adding LunchList

---
+++ LunchList/src/apt/tutorial/LunchList.java
@@ -0,0 +1,36 @@
+package apt.tutorial;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LunchList extends Activity {
+	
+	Restaurant r = new Restaurant();
+	
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_list);
+        
+        Button save = (Button) findViewById(R.id.save);
+        
+        save.setOnClickListener(onSave);
+    }
+
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+			
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+		}
+	};
+}
---
+++ LunchList/src/apt/tutorial/Restaurant.java
@@ -0,0 +1,21 @@
+package apt.tutorial;
+
+public class Restaurant {
+
+	private String name = "";
+	private String address = "";
+	
+	public  String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return this.address;
+	}
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+}
