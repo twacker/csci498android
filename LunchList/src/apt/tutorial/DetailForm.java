9ef51b593c8ead730b841763e8fbbc528be9bf5a
Taylor Wacker <twacker@mymail.mines.edu>
2012-10-01 09:48:26 -0600

    added empty DetailForm activity ch12 steps 1,2

---
+++ LunchList/src/apt/tutorial/DetailForm.java
@@ -0,0 +1,11 @@
+package apt.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+	}
+}
