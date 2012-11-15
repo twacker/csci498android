e74eddde4781afed7ea0e99f2707ee1b2cdbe24e
Taylor Wacker <twacker@mymail.mines.edu>
2012-11-15 15:40:05 -0700

    added ringtone to notification ch 27

--- LunchList/src/apt/tutorial/OnAlarmReceiver.java
+++ LunchList/src/apt/tutorial/OnAlarmReceiver.java
@@ -4,6 +4,8 @@ import android.app.*;
 import android.content.*;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.media.AudioManager;
+import android.net.Uri;
 
 public class OnAlarmReceiver extends BroadcastReceiver {
 
@@ -12,13 +14,18 @@ public class OnAlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive( Context ctxt, Intent intent ) {
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( ctxt );
-		boolean useNotification = prefs.getBoolean( "use-notification", true );
+		boolean useNotification = prefs.getBoolean( "use_notification", true );
 		if ( useNotification ) {
 			NotificationManager mgr = ( NotificationManager ) ctxt.getSystemService( Context.NOTIFICATION_SERVICE );
 			Notification note = new Notification( R.drawable.stat_notify_chat, "It's time for lunch!", System.currentTimeMillis() );
 			PendingIntent i = PendingIntent.getActivity( ctxt, 0, new Intent( ctxt, AlarmActivity.class ), 0 );
 			note.setLatestEventInfo( ctxt, "LunchList", "It's time for lunch! Aren't you hungry?", i );
 			note.flags |= Notification.FLAG_AUTO_CANCEL;
+			String sound = prefs.getString( "alarm_ringtone", null );
+			if ( sound != null ) {
+				note.sound = Uri.parse( sound );
+				note.audioStreamType = AudioManager.STREAM_ALARM;
+			}
 			mgr.notify( NOTIFY_ME_ID, note );
 		} else {
 			Intent i = new Intent( ctxt, AlarmActivity.class );
